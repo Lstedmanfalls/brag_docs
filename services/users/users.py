d2aa3d2312b17938892d5bddd38c0bab72fae38d
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2023-12-19 08:31:06 -0700

    type hint service functions

--- services/users/users.py
+++ services/users/users.py
@@ -1,8 +1,8 @@
 from models.user import User
 
-def create_user(first_name, last_name):
+def create_user(first_name: str, last_name: str) -> str:
     new_user = User(first_name, last_name)
     return f'Added {new_user}'
 
-def get_users():
+def get_users() -> list:
     return User.all
